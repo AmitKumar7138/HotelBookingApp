0c84fc69426f045de7ea7b957372828969b65f7f
AmitKumar7138 <amitprataprana41@gmail.com>
2024-01-26 13:31:04 -0500

    Add Homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,7 @@ import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 
 const App = () => {
@@ -25,7 +26,7 @@ const App = () => {
         <Route path="/" element={
 
           <Layout>
-            <p>Homepage</p>
+            <Home />
           </Layout>
         } />
 
